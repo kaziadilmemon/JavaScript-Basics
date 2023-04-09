be9961ad3219ba3844598218139766e0504ae93b
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-04-09 18:27:54 +0500

    Create Strings in JavaScript.js

---
+++ Strings in JavaScript.js
@@ -0,0 +1,19 @@
+// Strings are used to store and mnaipulate text.
+let name="Harry"
+// We can Write Strings as in single quoted as let name='harry'
+
+/*                     Template Literals
+Template Literals are use backtics instead of quotes to define.*/
+let name1="Adil"
+let name2="Asim"
+// now we can print like Adil is a friend of Asim
+let sentence=`${name1} is a friend of ${name2}`
+console.log(sentence)
+
+ // We can insert variables directly in template literals This is called String interpolation.
+
+
+//      Escape Sequence:
+  let fruit='Bana\'na'
+console.log(fruit.length)
+// We have other Escape Sequence too which we can use there like  \n=newline , \t=Tab , \r=Carriage Return
